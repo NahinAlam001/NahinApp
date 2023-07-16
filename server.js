901a587b3f213992462cf1ec2373123bc318d3af
Nahin <nahin.alam@northsouth.edu>
2023-07-17 00:01:02 +0600

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,1 @@
-import express from "express"
-import { colors } from 'colors'
-//rest object
-const app = express()
-
-//rest api
-app.get('/',(req,res)=>{
-    res.send('<h1>Welcome Home</h1>')
-})
-
-//Port
-const PORT = 8080
-
-//run listen
-app.listen(PORT, ()=>{
-console.log(`Server running on ${PORT}`.bgCyan.white)
-})
+import { express } from "express";
