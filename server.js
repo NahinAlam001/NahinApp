1bef52f1124676eeffae33baab5a95e17fade95d
Nahin <nahin.alam@northsouth.edu>
2023-07-17 11:47:37 +0600

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-import { express } from "express";
+import express from 'express';
+import colors from 'colors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hi</h1>")
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () =>{
+    console.log(`Server running on ${PORT} in ${process.env.MOD}`.bgMagenta.white)
+})
